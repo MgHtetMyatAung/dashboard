4672d94a542d2f0658fa094702e2376b886e740f
MgHtetMyatAung <htetmyat@innovix-solutions.com>
2024-12-19 17:22:50 +0630

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import DashboardLayout from "./components/layout/DashboardLayout";
+
+export default function App() {
+  return (
+    <DashboardLayout>
+      <div>App</div>
+    </DashboardLayout>
+  );
+}
